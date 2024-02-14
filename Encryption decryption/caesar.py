adb018c670d88eff60ce78c9c75bebecfe9a5809
nivagion <cucakleo@gmail.com>
2024-02-14 17:08:55 +0100

    added z to the alphabet shoutout cylo

--- Encryption decryption/caesar.py
+++ Encryption decryption/caesar.py
@@ -7,7 +7,7 @@ def encrypt(text, shift):
         char = text[i]
         if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':#ili ovako .isupper()
             result += chr((ord(char) + shift - 65) % 26 + 65) #ord> char to number, 65> range between 0-26
-        elif char in 'abcdefghijklmnopqrstuvwxy':#ili ovako .islower()
+        elif char in 'abcdefghijklmnopqrstuvwxyz':#ili ovako .islower()
             result += chr((ord(char) + shift - 97) % 26 + 97)
         else:
             result += char
